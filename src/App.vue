e7395db0f4803d0c7caad6c970b9dd7300ea9f07
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-28 20:13:34 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Discover</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+          <v-card>
+            <v-img :src="card.src" class="white--text align-end" height="200px">
+            </v-img>
+
+            <v-card-actions>
+              <v-row>
+                <v-col>
+                  <span>100 บาท </span>
+                </v-col>
+                <v-col>
+                  <v-btn color="orange" class="ml-3">
+                    ซื้อ
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    cards: [
+      {
+        title: "Favorite road trips",
+        src: "https://source.unsplash.com/1600x900/?food",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://source.unsplash.com/1600x900/?food2",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://source.unsplash.com/1600x900/?food3",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://source.unsplash.com/1600x900/?food4",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://source.unsplash.com/1600x900/?food5",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://source.unsplash.com/1600x900/?food6",
+        flex: 6,
+      },
+    ],
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
