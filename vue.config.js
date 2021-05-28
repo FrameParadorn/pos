b69a38b4a47a462ad359bc7cf8217114a73c7a7e
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-28 21:18:07 +0700

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/pos/" : "/",
   transpileDependencies: ["vuetify"],
+  devServer: {
+    disableHostCheck: true,
+  },
 };
